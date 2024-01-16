c349a02b70cd893a97c7f565707899740f60667c
Nicolas Gheeraert <n.gheeraert@physics.iitm.ac.in>
2024-01-16 17:47:11 +0530

    added files

---
+++ drm/__init__.py
@@ -0,0 +1,6 @@
+import numpy as np
+from .functions import *
+from .system_class import system
+import matplotlib.pyplot as plt
+
+pi = np.pi
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='drm',
+      version='0.1',
+      description='This program simulates the driven rabi model using master equation integration.',
+      long_description=readme(),
+      author='Nicolas Gheeraert',
+      author_email='n.gheeraert@physics.iitm.ac.in',
+      license='',
+      packages=['drm'],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],)
